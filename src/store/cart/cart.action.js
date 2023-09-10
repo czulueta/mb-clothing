976c2511a1e22ffabe20a4facf4b389896aae9e0
Cesar Zulueta <cesarzulueta12@gmail.com>
2023-09-10 13:15:47 -0700

    converted checkout item and checkout component over to redux and deleted context completely

--- src/store/cart/cart.action.js
+++ src/store/cart/cart.action.js
@@ -41,7 +41,7 @@ export const addItemToCart = (cartItems, productToAdd) => {
   const newCartItems = addCartItem(cartItems, productToAdd);
   return createAction(CART_ACTION_TYPES.SET_CART_ITEMS, newCartItems);
 };
-export const removeItemToCart = (cartItems, cartItemToRemove) => {
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
   const newCartItems = removeCartItem(cartItems, cartItemToRemove);
   return createAction(CART_ACTION_TYPES.SET_CART_ITEMS, newCartItems);
 };
